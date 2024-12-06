93a3f8f4a8cac905727070dbcbe4c3e1aea24f0a
Michael Turkstra <mturkst2@uwo.ca>
2024-12-05 19:57:48 -0500

    Added Animation and small changes
    Added animation function to visualize results, changed the probability to 2d, removed redundant part of make tridiagonal, changed H matrix declaration

--- TurkstraMichael_Project4.py
+++ TurkstraMichael_Project4.py
@@ -1,5 +1,7 @@
 import numpy as np
 import numpy.linalg as nplinalg
+import matplotlib.pyplot as plt
+from celluloid import Camera
 
 h_bar, m = 1, 1/2
 
@@ -7,27 +9,14 @@ def sch_init_cond(x_points, sig_0, k_0, x_0):
     init_cond = (1/np.sqrt(sig_0)*np.sqrt(np.pi))*np.exp((k_0*x_points*1j) - ((x_points-x_0)**2)/((2*sig_0)**2))
     return init_cond
 
-def make_tridiagonal(Nspace, Ntime, b, d, a):                                                                               ## From Lab 10
+def make_tridiagonal(Nspace, b, d, a):                                                                               ## From Lab 10
     '''
     Function to make matrix following format from lab outline
     Args: Takes in the L length of matrix to make, b value of elements on diagonal-1, d value of elements on diagonal, a value of elements on diagonal+1
     Returns: Matrix that was made
     '''
-    if (Nspace > Ntime):
-        D = np.zeros((Nspace, Ntime))
-        for i in range(Ntime):
-            D[i, i] = d
-            D[(i + 1)%Nspace, i] = a
-            D[(i - 1)%Nspace, i] = b
-    elif (Nspace < Ntime):
-        D = np.zeros((Nspace, Ntime))
-        for i in range(Ntime):
-            D[i%Nspace, i] = d
-            D[(i + 1)%Nspace, i] = a
-            D[(i - 1)%Nspace, i] = b
-    else:
-        D = d*np.identity(Nspace)+a*np.diagflat(np.ones(Nspace-1),1)+b*np.diagflat(np.ones(Nspace-1),-1)                       ## Equation for making required matrix, d*diagonal + b*diagonal-1, + a*diagonal+1, takes insperation from “lecture10a-FTCS-Diffusion_mtx.ipynb”
-        D[Nspace-1, 0], D[0, Nspace-1] = b, a
+    D = d*np.identity(Nspace)+a*np.diagflat(np.ones(Nspace-1),1)+b*np.diagflat(np.ones(Nspace-1),-1)                       ## Equation for making required matrix, d*diagonal + b*diagonal-1, + a*diagonal+1, takes insperation from “lecture10a-FTCS-Diffusion_mtx.ipynb”
+    D[Nspace-1, 0], D[0, Nspace-1] = b, a
     return D                                                                                                    ## Return matrix
 
 
@@ -46,29 +35,45 @@ def sch_eqn(nspace, ntime, tau, method='ftcs', length=200, potential=[], wparam=
     psi = np.zeros((nspace, ntime), dtype=np.complex128)
     psi[:, 0] = init_cond
 
-    prob = np.zeros(ntime)
-    prob[0] = np.sum(np.abs(psi[:, 0])**2)*h*tau
+    prob = np.zeros((nspace, ntime), dtype=np.complex128)
+    prob[:, 0] = np.conjugate(psi[:, 0]).dot(psi[:, 0])*h*tau
 
-    H = make_tridiagonal(nspace, ntime, (-(h_bar**2)/(2*m*(h**2))), (2*(h_bar**2)/(2*m*(h**2)))+V, (-(h_bar**2)/(2*m*(h**2))))
+    #H = make_tridiagonal(nspace, (-(h_bar**2)/(2*m*(h**2))), (2*(h_bar**2)/(2*m*(h**2)))+V, (-(h_bar**2)/(2*m*(h**2))))
+
+    H = -((h_bar**2)/2*m)*make_tridiagonal(nspace, 1/(h**2), -2/(h**2), 1/(h**2)) + V*np.identity(nspace)
 
     if (method == 'ftcs'):
         for i in range(ntime-1):
-            psi[:, i+1] = (make_tridiagonal(nspace, ntime, 0, 1, 0) - (1j*tau/h_bar)*H).dot(psi[:, i])
-            prob[i+1] = np.sum(np.abs(psi[:, i+1])**2)*h*tau
+            psi[:, i+1] = (make_tridiagonal(nspace, 0, 1, 0) - (1j*tau/h_bar)*H).dot(psi[:, i])
+            #prob[i+1] = np.sum(np.abs(psi[:, i+1])**2)*h*tau
+            prob[:, i+1] = np.conjugate(psi[:, i+1]).dot(psi[:, i+1])*h*tau
     elif (method == 'crank'):
         for i in range(ntime-1):
-            psi[:, i+1] = nplinalg.inv(make_tridiagonal(nspace, ntime, 0, 1, 0) + (1j*tau/(2*h_bar))*H).dot(make_tridiagonal(nspace, ntime, 0, 1, 0) - (1j*tau/(2*h_bar))*H).dot(psi[:, i])
-            prob[i+1] = np.sum(np.abs(psi[:, i+1])**2)*h*tau    
+            psi[:, i+1] = nplinalg.inv(make_tridiagonal(nspace, 0, 1, 0) + (1j*tau/(2*h_bar))*H).dot(make_tridiagonal(nspace, 0, 1, 0) - (1j*tau/(2*h_bar))*H).dot(psi[:, i])
+            #prob[i+1] = np.sum(np.abs(psi[:, i+1])**2)*h*tau
+            prob[:, i+1] = np.conjugate(psi[:, i+1]).dot(psi[:, i+1])*h*tau
     else:
         print("Invalid Method\n")
     
+    #prob = psi.dot(np.conjugate(psi))
+
     return psi, x_points, t_points, prob
+
+def sch_animate(psi, x_points, t_points,):
+    fig = plt.figure()
+    camera = Camera(fig)
+    for i in range(0, len(t_points), 5):
+        plt.plot(x_points, psi[:, i], color='blue', label=f"Schrodinger Eq at t = {t_points[i]}")
+        camera.snap()                                                                                   ## Capturing frame at current time point
+    animation = camera.animate()                                                                        ## Animating frames together 
+    animation.save("TurkstraMichael_Lab7_coupled_oscillators.mp4",fps=60)                               ## Saving animation
+    
 
 
 def main():
-    nspace, ntime, tau = 100, 100, 0.1
-    psi, x_grid, t_grid, prob = sch_eqn(nspace, ntime, tau, 'crank', potential=[np.arange(0, 50, 1)])
-    print(psi)
+    nspace, ntime, tau = 100, 50000, 0.1
+    psi, x_grid, t_grid, prob = sch_eqn(nspace, ntime, tau, 'crank', potential=[80])
+    sch_animate(psi, x_grid, t_grid)
     print(prob)
 
 if __name__ == '__main__':
